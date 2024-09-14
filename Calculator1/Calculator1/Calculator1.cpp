019d858b9937e5849ec9740fdb2c19c566440f2c
Whyumadkiddo <124880624+Whyumadkiddo@users.noreply.github.com>
2024-09-14 21:49:08 +0300

    Add files via upload

---
+++ Calculator1/Calculator1/Calculator1.cpp
@@ -0,0 +1,49 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double firstnum;
+	double secondnum;
+	double result;
+	char simbol;
+	{
+		while (true) {
+			std::cout << "Напишите первое число: \n";
+			std::cin >> firstnum;
+
+			std::cout << "Напишите символ: + - * / \n";
+			std::cin >> simbol;
+
+			std::cout << "Напишите второе число: \n";
+			std::cin >> secondnum;
+
+			switch (simbol)
+			{
+			case '+':
+				result = firstnum + secondnum;
+				std::cout << "Ваш результат равен " << result << "\n\n";
+				break;
+			case '-':
+				result = firstnum - secondnum;
+				std::cout << "Ваш результат равен " << result << "\n\n";
+				break;
+			case '*':
+				result = firstnum * secondnum;
+				std::cout << "Ваш результат равен " << result << "\n\n";
+				break;
+			case '/':
+				result = firstnum / secondnum;
+				std::cout << "Ваш результат равен " << result << "\n\n";
+				break;
+
+			default:
+				std::cout << "Произошла ошибка \n";
+				break;
+			}
+		}
+	}
+}
+
